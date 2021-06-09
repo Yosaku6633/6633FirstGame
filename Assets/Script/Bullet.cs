0082561401483f6d64dd4dd0cca1b7e05fce1d7c
Hongshan Fan <fanhongshan33@gmail.com>
2021-06-10 00:03:07 +0100

    发射子弹

---
+++ Assets/Script/Bullet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float step = 3f * Time.deltaTime;
+        transform.Translate(0, step, 0);
+
+        Vector3 sp = Camera.main.WorldToScreenPoint(transform.position);
+        if (sp.y > Screen.height)
+        {
+            GameObject.Destroy(this.gameObject);
+        }
+
+    }
+}
---
+++ Assets/Script/shoot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shoot : MonoBehaviour
+{
+    public GameObject myPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            GameObject bullet = Instantiate(myPrefab);
+            bullet.transform.position = transform.position + new Vector3(0, 1f, 0);
+        }
+        
+    }
+}
