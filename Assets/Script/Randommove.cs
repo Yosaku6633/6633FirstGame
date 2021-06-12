7e139106a0efcff87aec663692074cfa5d0fc409
Yosaku <yangcuo8@126.com>
2021-06-12 21:22:16 +0800

    DestroyEntity

---
+++ Assets/Script/Randommove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Randommove : MonoBehaviour
+{
+    
+        public float tumble;
+        private Rigidbody rb;
+
+        void Start()
+        {
+            rb = GetComponent<Rigidbody>();
+            rb.angularVelocity = Random.insideUnitSphere * tumble;
+        }
+    
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
